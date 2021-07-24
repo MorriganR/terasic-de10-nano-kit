a47fcc203cb3246fbe70168d216633ddf8e9a0e5
MorriganR <dasha@ddyka.com>
2021-07-24 16:12:03 +0300

    simplify

--- .docker/play-in-docker.sh
+++ .docker/play-in-docker.sh
@@ -15,10 +15,25 @@ echo "# ls -la" && ls -la
 
 echo "# env" && env
 
-echo "apt-get update && apt-get install --no-install-recommends ubuntu-desktop -y" && \
-apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends ubuntu-desktop
+apt-get update && apt-get install -y libtcmalloc-minimal4 libglib2.0-0
 
-export PATH="/root/intelFPGA_lite/20.1/quartus/bin:$PATH"
+#echo "apt-get update && apt-get install --no-install-recommends ubuntu-desktop -y" && \
+#apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends ubuntu-desktop
+
+
+export Q_INST_DIR=/root/intelFPGA_lite/20.1
+
+export QUARTUS_ROOTDIR=${Q_INST_DIR}/quartus
+export QUARTUS_ROOTDIR_OVERRIDE=${Q_INST_DIR}/quartus
+export SOPC_KIT_NIOS2=${Q_INST_DIR}/nios2eds
+
+export PATH=${Q_INST_DIR}/quartus/sopc_builder/bin/:$PATH
+export PATH=${Q_INST_DIR}/quartus/bin/:$PATH
+export PATH=${Q_INST_DIR}/nios2eds/:$PATH
+export PATH=${Q_INST_DIR}/nios2eds/bin/:$PATH
+export PATH=${Q_INST_DIR}/nios2eds/sdk2/bin/:$PATH
+export PATH=${Q_INST_DIR}/nios2eds/bin/gnu/H-x86_64-pc-linux-gnu/bin/:$PATH
+export PATH=${Q_INST_DIR}/quartus/linux64/gnu/:$PATH
 
 echo "# env" && env
 
@@ -27,3 +42,17 @@ echo "# /root/intelFPGA_lite/20.1/quartus/bin/quartus_sh --version" && \
 
 echo "# quartus_sh --version"&& \
 quartus_sh --version || :
+
+cd /usr/src/de10nano/tutorials/MyFirstFPGA
+echo "# pwd" && pwd
+echo "# ls -la" && ls -la
+echo "# ./build_example.sh"
+./build_example.sh || :
+echo "# ls -hlaR ./blink" && ls -hlaR ./blink
+cat ./blink/db/blink.db_info
+cd ./blink
+
+quartus_sh --flow compile blink || :
+
+echo "# pwd" && pwd
+echo "# ls -hlaR" && ls -hlaR
--- .docker/quartus.dockerfile
+++ .docker/quartus.dockerfile
@@ -1,24 +1,23 @@
 FROM ubuntu:20.04
 
 ARG DEBIAN_FRONTEND=noninteractive
-RUN apt-get update && apt-get install -y curl expect locales
+RUN apt-get update && apt-get install -y curl locales
 
 RUN sed -i '/en_US.UTF-8/s/^# //g' /etc/locale.gen && locale-gen
 ENV LANG en_US.UTF-8
 ENV LANGUAGE en_US:en
 ENV LC_ALL en_US.UTF-8
 
-RUN mkdir /qinst && mkdir /qinst/components
-COPY .docker/*.sh /qinst/
+RUN apt-get install -y --no-install-recommends ubuntu-desktop
 
-RUN cd /qinst/components &&\
+# add Cyclone IV
+#  curl -O https://download.altera.com/akdlm/software/acdsinst/20.1std.1/720/ib_installers/cyclone-20.1.1.720.qdz &&\
+RUN mkdir /qinst &&\
+  cd /qinst &&\
   curl -O https://download.altera.com/akdlm/software/acdsinst/20.1std.1/720/ib_installers/QuartusLiteSetup-20.1.1.720-linux.run &&\
-  curl -O https://download.altera.com/akdlm/software/acdsinst/20.1std.1/720/ib_installers/cyclone-20.1.1.720.qdz &&\
   curl -O https://download.altera.com/akdlm/software/acdsinst/20.1std.1/720/ib_installers/cyclonev-20.1.1.720.qdz &&\
   chmod +x *.run &&\
-  cd /qinst &&\
-  chmod +x * &&\
-  ./play-setup.sh &&\
+  ./QuartusLiteSetup-20.1.1.720-linux.run --mode unattended --unattendedmodeui minimal --installdir /root/intelFPGA_lite/20.1 --accept_eula 1 &&\
   cd / &&\
   rm -rf /qinst
 
